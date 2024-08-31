10abb1144eb23dbd6a7580efef014af107910b8f
Pushkekarr <wolfrohitth@gmail.com>
2024-08-31 12:44:25 -0500

    arrays

--- 07-array.sh
+++ 07-array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FRUITS=("APPLE", "KIWI", "ORANGE")
+FRUITS=("APPLE"  "KIWI" "ORANGE")
 echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
