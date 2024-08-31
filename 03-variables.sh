3476ce07b29d7fc2b7856093fe1d37f198a75678
Pushkekarr <wolfrohitth@gmail.com>
2024-08-31 11:54:03 -0500

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,4 +5,4 @@ PERSON2=Suresh
 echo "$PERSON1:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: Hello $PERSON1.I am fine. How are you doing?"
 echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
-echo "${PERSON2}:: I started learning Shell Scripting $PERSON1h"
+echo "${PERSON2}:: I started learning Shell Scripting $PERSON1"
